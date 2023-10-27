e4b1c299b8289ef30a7b2b15bacc5a5f4f18c4b0
pwbenjaminhuang <bjhuang20000315@gmail.com>
2023-10-26 23:54:51 -0700

    translated comments to English

--- src/moving/src/move_arm.py
+++ src/moving/src/move_arm.py
@@ -17,31 +17,31 @@ class move_arm_node:
 
 
     def __init__(self):
-        # 初始化move_group的API
+        # initialize move_group API
         moveit_commander.roscpp_initialize(sys.argv)
 
-        # 初始化需要使用move group控制的机械臂中的arm group
+        # initialize arm group in the manipulator controlled by the move group. (初始化需要使用move group控制的机械臂中的arm group)
         self.arm = MoveGroupCommander('sagittarius_arm')
         self.gripper = MoveGroupCommander('sagittarius_gripper')
 
-        # 当运动规划失败后，允许重新规划
+        # when motion planning fails, allow replanning
         self.arm.allow_replanning(True)
         
-        # 设置目标位置所使用的参考坐标系
+        # set reference coordinate frame of the goal position
         reference_frame = 'world'
         self.arm.set_pose_reference_frame(reference_frame)
-        self.gripper.set_pose_reference_frame(reference_frame) #夹爪
+        self.gripper.set_pose_reference_frame(reference_frame) # gripper
                 
-        # 设置位置(单位：米)和姿态（单位：弧度）的允许误差
+        # set error tolerance for positions (in meter) and pose (in radian)
         self.arm.set_goal_position_tolerance(0.01)
         self.arm.set_goal_orientation_tolerance(0.01)
         self.arm.set_goal_joint_tolerance(0.02)
 
-        # 设置允许的最大速度和加速度
+        # set maximum allowed veloity and acceleration
         self.arm.set_max_acceleration_scaling_factor(0.5)
         self.arm.set_max_velocity_scaling_factor(0.5)
         
-        # 获取终端link的名称
+        # get end-effector link name
         self.end_effector_link = self.arm.get_end_effector_link()
 
         # Put arm in initial pose
@@ -68,30 +68,30 @@ class move_arm_node:
             waypoints.append(deepcopy(wpose))
         
 
-        fraction = 0.0   #路径规划覆盖率
-        maxtries = 100   #最大尝试规划次数
-        attempts = 0     #已经尝试规划次数
-        # 设置机器臂当前的状态作为运动初始状态
+        fraction = 0.0   # trajectory planning cover rate 路径规划覆盖率
+        maxtries = 100   # maximum allowed planning tries 最大尝试规划次数
+        attempts = 0     # number of tries attempted 已经尝试规划次数
+        # set current robot arm state as initial state 设置机器臂当前的状态作为运动初始状态
         self.arm.set_start_state_to_current_state()
-        # 尝试规划一条笛卡尔空间下的路径，依次通过所有路点，完成圆弧轨迹
+        # plan a cartesian path trajectory that pass all waypoints 尝试规划一条笛卡尔空间下的路径，依次通过所有路点，完成圆弧轨迹
         while fraction < 1.0 and attempts < maxtries:
             (plan, fraction) = self.arm.compute_cartesian_path (
                                     waypoints,   # waypoint poses，路点列表
                                     0.01,        # eef_step，终端步进值
                                     0.0,         # jump_threshold，跳跃阈值
                                     True)        # avoid_collisions，避障规划
-            # 尝试次数累加
+            # attempts + 1 
             attempts += 1
-            # 打印运动规划进程
+            # print trajectory planning progress 
             if attempts % 10 == 0:
                 rospy.loginfo("Still trying after " + str(attempts) + " attempts...")
                      
-        # 如果路径规划成功（覆盖率100%）,则开始控制机械臂运动
+        # if planning success (cover rate 100%), start moving 
         if fraction == 1.0:
             rospy.loginfo("Path computed successfully. Moving the self.arm.")
             self.arm.execute(plan)
             rospy.loginfo("Path execution complete.")
-        # 如果路径规划失败，则打印失败信息
+        # if planning fails, print failure message 
         else:
             rospy.loginfo("Path planning failed with only " + str(fraction) + " success after " + str(maxtries) + " attempts.")  
 
@@ -111,12 +111,12 @@ class move_arm_node:
         self.gripper.go()
         rospy.sleep(2)
 
-        # 控制机械臂先回到初始化位置
+        # let robot arm go back to initial state
         self.arm.set_named_target('sleep')
         self.arm.go()
         rospy.sleep(1)
         
-        # 关闭并退出moveit
+        # close moveit and exit
         moveit_commander.roscpp_shutdown()
         moveit_commander.os._exit(0)
 
@@ -136,7 +136,7 @@ class move_arm_node:
 if __name__ == "__main__":
 
     try:
-        # 初始化ROS节点
+        # initialize ROS nodes
         rospy.init_node('move_arm_node', anonymous=True)
 
         rospy.wait_for_message("/start_topic", String)
